b736301743b8b49ceddf462cc85e6484ea8b0341
inconnuboyxtech <xinconnuboydawens@gmail.com>
2025-04-21 11:00:03 -0300

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -32,7 +32,7 @@ const ping = async (m, Matrix) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363391979107532@newsletter',
+          newsletterJid: '120363397722863547@newsletter',
           newsletterName: "INCONNU BOY",
           serverMessageId: 143
         }
